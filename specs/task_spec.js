7450fe343220ec8e1db35766a3e3bddffe97849e
Marta <martadabrowka@gmail.com>
2017-12-08 10:50:43 +0000

    passed task has a reward

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -16,6 +16,10 @@ it('task should have a difficulty level', function() {
   assert.strictEqual(brushTeeth.difficultyLevel, 2);
 })
 
-it('task should have an unrgency level', function() {
+it('task should have an urgency level', function() {
   assert.strictEqual(brushTeeth.urgencyLevel, 3);
+})
+
+it('task should have a reward ', function() {
+  assert.strictEqual(brushTeeth.reward, "breakfast");
 })
