19e2a80e2e5ab8fc1fbb969a4bef0b7ac11fcf1a
Marta <martadabrowka@gmail.com>
2017-12-08 11:00:44 +0000

    passed food should have a name

---
+++ specs/food_spec.js
@@ -0,0 +1,17 @@
+const Food = require("../food.js")
+const assert = require('assert')
+
+
+describe("Food", function () {
+  let food1;
+})
+
+
+beforeEach(function() {
+  food1 = new Food("Eggs Benedict", 5);
+})
+
+
+it('food should have a name', function() {
+  assert.strictEqual(food1.name, "Eggs Benedict")
+})
